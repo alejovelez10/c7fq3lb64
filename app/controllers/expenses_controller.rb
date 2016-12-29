cb2c3df3c2d4909f4b3c30aa0f999a1de140a68f
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2016-12-29 14:44:53 -0500

    solucion

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,11 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+  	 @concept = params[:concept]
+  	 @category = params[:category_id]
+  	 puts @concept
+  	 puts @category
+     @expenses = Expense.where("category_id LIKE ? AND concept LIKE ?" ,"%#{@category}%", "%#{@concept}%")
   end
 end
+
+
