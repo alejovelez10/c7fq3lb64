381321b0372d226f6c6ca94b6689da0368df57cd
alejosaldarriagaalzate <alejoalzate@misena.edu.co>
2017-01-04 14:27:36 -0500

    Nueva solucion

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,26 @@
 class ExpensesController < ApplicationController
   def index
+        
+    if !params[:concept].blank?
 
-  	 @expenses = Expense.where("category_id LIKE ? AND concept LIKE ?" ,"%#{params[:category_id]}%", "%#{params[:concept]}%")
+  	 @expenses = Expense.where("concept LIKE ? " ,"%#{params[:concept]}%")
+       
+
+  	else
+
+  		@expenses = Expense.all
+    
+    end
+
+    if !params[:category_id].blank?
+
+  	 @expenses = @expenses.where(category_id: params[:category_id])
+  
+
+
+  		
+    end
+
   end
 end
-
 
